852207f34abf872a9614b792ea9d0b2b5894e74d
Koi-beth <wanguibeth967@gmail.com>
2024-03-25 21:25:10 +0300

    drag and drop

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -20,7 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
-
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -43,7 +44,7 @@ function updateSavedColumns() {
   listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
   const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
   arrayNames.forEach((arrayName, index) => {
-     localStorage.setItem(`$${arrayName}Items`, JSON.stringify(listArrays[index]));
+     localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
   });
 }
 
@@ -57,6 +58,9 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)');
+
   // Append
   columnEl.appendChild(listEl);
 
@@ -92,6 +96,38 @@ createItemEl(backlogList, 0, backlogItem, index);
 
 
 }
+
+// When Items start Dragging
+function drag(e) {
+  draggedItem = e.target;
+console.log('draggedIteem:', draggedItem);
+}
+
+// Columns Allow for Items to Drop
+function allowDrop(e) {
+ e.preventDefault();
+}
+
+// When Item Enters Column Area
+function dragEnter(column) {
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+
+// Dropping Item in Column
+function drop(e) {
+  e.preventDefault();
+  // Remove Background Color/Padding
+  listColumns.forEach((column) => {
+      column.classList.remove('over');
+  });
+
+  // Add Item to Column
+  const parent = listArrays[currentColumn];
+  parent.appendChild(draggedItem);
+}
+
 
 // On Load 
 updateDOM();
